98141bb5e0c3c6545df777a0852df27c0562aba1
abhamra <33864884+abhamra@users.noreply.github.com>
2020-08-08 20:25:52 -0400

    Updated script.js with firebase code and firebase for loop
    + action items

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,52 @@
 
+var db = firebase.firestore()
 
-db.collection('editions').get().then((snapshot)=>{
 
-    snapshot.docs.forEach(doc => {
-        console.log(doc.id, "=> ",);
 
-// db.collection('editions').doc('edition15').collection('title').doc('title').data().header
-    })
 
-});
+
+// db.collection("editions").get().then(function(querySnapshot) {
+//     querySnapshot.forEach(function(doc) {
+//         // doc.data() is never undefined for query doc snapshots
+//         //console.log(doc.id);
+        
+//         console.log(createEdition(doc.id));
+
+//     });
+// });
+
+//create edition will add everything/basically create the entire page
+function createEdition(edition){
+
+    db.collection("editions").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            //console.log(doc.id);
+            
+            console.log(createEdition(doc.id));
+    
+        });
+    });
+
+    var main = document.getElementById('15');
+
+    var div = document.createElement("div");
+    div.textContent = edition;
+
+   main.appendChild(div);
+    
+}
+/*
+
+what we have:
+- have 3 hardcoded options on the main page (more, less, whatever)
+- on click (any of them), will redirect to new page
+- on new page, will be a diff html file
+- will generate the new edition (create edition) based on the document id
+- (add firebase imports to the new file, yay)
+- 
+
+*/
+
+
 
