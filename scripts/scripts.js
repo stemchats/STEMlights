4a765de7999c8823446d9b24ed9938d438660611
abhamra <33864884+abhamra@users.noreply.github.com>
2020-08-13 15:50:38 -0400

    Adding Ideas/Action Items to Script.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,31 +7,9 @@ hide = () => {
    editionSection.innerHTML = "";
 }
 const collection = document.getElementsByClassName("editions");
-//selects all [anchor] elements with class "editions"
-// const array = [];
-// array.push(...collection);
-// //push collection to an arry
-// array.forEach(event);
-// //add eventlistener to each anchor tag (element of the array)
-// function event(value, i) {
-//   value.addEventListener("click", function(e) {
-//     e.preventDefault();
-//     createEdition(value.id);
-//   });
-// }
 
-// db.collection("editions").get().then(function(querySnapshot) {
-//     querySnapshot.forEach(function(doc) {
-//         // doc.data() is never undefined for query doc snapshots
-//         //console.log(doc.id);
 
-//         console.log(createEdition(doc.id));
 
-//     });
-// });
-//
-//
-// //create edition will add everything/basically create the entire page
 
 var sections = ["investemgations", "opportunities", "news"];
 var fields = ["paragraph1", "paragraph2", "paragraph3"];
@@ -62,8 +40,6 @@ db.collection("editions").get().then(function(querySnapshot) {
 });
 function createEdition(edition){
 
-      
-
      //  var main = document.getElementById('editionId');
      //
      //  var div = document.createElement("div");
@@ -73,11 +49,23 @@ function createEdition(edition){
 }
 /*
 
+
+
 what we have:
 - have 3 hardcoded options on the main page (more, less, whatever)
 - on click (any of them), will redirect to new page
 - on new page, will be a diff html file
 - will generate the new edition (create edition) based on the document id
 - (add firebase imports to the new file, yay)
--
+
+
+8/13/2020
+
+- rename each field alphanumerically for each field in each section
+  - e.g (1) header, (2), image1, etc.
+- add a sections array to each html file with the unique order of the sections in the given edition
+  - e.g for edition15, var sections = [title, investemgations, politics, challenges, etc.]
+- creating the individual functions for each element type
+- using a parse function to find out which element function to call 
+  - e.g if we use (1) header, it should call the createHeader() function
 */
