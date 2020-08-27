efce0215a618556a9474c5800bae0e50d1529515
abhamra <33864884+abhamra@users.noreply.github.com>
2020-08-27 16:44:49 -0400

    Added search bar function and checking mechanism

--- scripts/scripts2.js
+++ scripts/scripts2.js
@@ -1,12 +1,20 @@
-function search(edition){
+function search(input){
+  var edition = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]; // update this later depending on # of completed editions (searches through them)
+  for(var j = 0; j<edition.length;j++){
     for(var i = 0;i<sections.length;i++){ //how many sections the edition has, iterates through sections (below)
-        db.collection("editions").doc("edition" + edition).collection(sections[i]).get()
-          .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-              const data = doc.data(); //retrieves all the sections as 'objects'
-              
-              
-            });
+      db.collection("editions").doc("edition" + edition[j]).collection(sections[i]).get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            const data = doc.data(); //retrieves all the sections as 'objects'
+            let entries1 = Object.entries(data); //return array of each object's key-value pairs
+            for (const [key, value] of entries1.sort()) { //loop through each element (key-value) in the array
+              if(value.includes(input)==true){
+                //create a card thingy ig?
+              }
+            }
           });
-      };
-    }
+        });
+    };
+  }
+  }
+    
