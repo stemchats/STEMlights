ff4ea85b5d179353b02cd35d7f5fc826c8bc03d7
abhamra <33864884+abhamra@users.noreply.github.com>
2020-08-27 20:31:43 -0400

    console.log ing things in script2.js

--- scripts/scripts2.js
+++ scripts/scripts2.js
@@ -1,3 +1,4 @@
+var sections = ["title", "investemgations", "politics", "voices", "news", "opportunities", "challenge", ];
 function search(input){
   var edition = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]; // update this later depending on # of completed editions (searches through them)
   for(var j = 0; j<edition.length;j++){
@@ -8,6 +9,7 @@ function search(input){
             const data = doc.data(); //retrieves all the sections as 'objects'
             let entries1 = Object.entries(data); //return array of each object's key-value pairs
             for (const [key, value] of entries1.sort()) { //loop through each element (key-value) in the array
+              console.log(value);
               if(value.includes(input)==true){
                 //create a card thingy ig?
               }
@@ -17,4 +19,5 @@ function search(input){
     };
   }
   }
+  search("hi");
     
