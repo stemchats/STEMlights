ec342f3269fb853ade14b0b7e7f95a305844f6a7
joseph.chiang20 <joseph.chiang20@gmail.com>
2021-04-10 14:07:19 -0700

    search bar changes commented

--- scripts/search.js
+++ scripts/search.js
@@ -1,55 +1,18 @@
-// 'use strict';
-//
-// const e = React.createElement;
-//
-// class LikeButton extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { liked: false };
-//   }
-//
-//   render() {
-//     if (this.state.liked) {
-//       return 'You liked this.';
-//     }
-//
-//     return e(
-//       'button',
-//       { onClick: () => this.setState({ liked: true }) },
-//       'Like'
-//     );
-//   }
-// }
-//
-// const domContainer = document.querySelector('#searchBar');
-//
-// ReactDOM.render(e(LikeButton), domContainer);
+const selectSearch = document.querySelector('.search-icon');
+const query = document.querySelector('.search-input');
 
-  class SearchForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-      alert(this.state.value);
-      event.preventDefault();
-    }
-
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-        <input type="search" class="search-input" placeholder="Search..." value={this.state.value} onChange={this.handleChange} name="search" autocomplete="off">
-        <input type="submit" class="search-submit" value="">
-        <span class="search-icon"><i class="fas fa-search"></i></span>
-        </form>
-      );
-    }
+// search input text on click
+selectSearch.addEventListener('click', (event) => {
+  search(query.value);
+})
+// search input text on enter
+query.addEventListener('keyup', (event) => {
+  if(event.keyCode === 13) {
+    search(query.value);
   }
+})
+
+//search function
+function search(queryString) {
+  console.log(`searched ${queryString}`);
+}
