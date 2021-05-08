8074f3dba1e3adac4f1608ebd6aa6a20ddcd021b
joseph.chiang20 <joseph.chiang20@gmail.com>
2021-05-07 17:05:12 -0700

    complete search functions, reduce big O*

--- scripts/search.js
+++ scripts/search.js
@@ -41,43 +41,35 @@ query.addEventListener('keyup', (event) => {
 //     }
 // }
 
-let editionsRef = db.collection('editions').doc('edition8').collection('title');
-
 const search = async (queryString) => {
-    const editions = [];
-    const returnEditions = [];
-    let allEditions = await editionsRef.get();
-    for(const doc of allEditions.docs){
-        //console.log(doc.id, '=>', doc.data());
-        // editions.push(doc.id);
-        console.log(doc.data())
+    const editions = []; // will contain array of all editions live in database
+      let editionsRef = db.collection('editions');
+      let allEditions = await editionsRef.get();
+      for(const doc of allEditions.docs){
+          editions.push(doc.id);
+      }
+      search2(editions, queryString);
     }
-    // editions is populated with all editions live in the database
 
-    console.log(queryString)
-
-    // for(let i = 0; i < editionsList.length; i++) { //iterate through each edition
-    //   for(let j=0; j < sectionsList.length; j++) { //iterate through each possible subcollection in each edition
-    //     let newEditions = await editionsRef.doc(editionsList[i]).collection(sectionsList[j]).get();
-    //     for(const doc of newEditions.docs){
-    //       const data = doc.data();
-    //       let entries1 = Object.entries(data); //return array of each object's key-value pairs
-    //       for (const [key, value] of entries1.sort()) { //loop through each element (key-value) in the array
-    //           let keyword = `${key}`; //the different key properties
-    //           let values = `${value}`; //the different value properties
-    //           if (values.includes(`${queryString}`) == true) {
-    //               returnEditions.push("edition" + editions[i]);
-    //           }
-    //       }
-    //     }
-    //   }
-    // }
-    // console.log(formatArray(returnEditions));
+const search2 = async (editionsList, queryString) => {
+    const returnEditions = [];
+    for(let i = 0; i < editionsList.length; i++) {
+      for(let j = 0; j < sectionsList.length; j++) {
+        let editionsRef = db.collection('editions').doc(editionsList[i]).collection(sectionsList[j]);
+        let allEditions = await editionsRef.get();
+        for(const doc of allEditions.docs){
+            const data = doc.data();
+            let entries1 = Object.entries(data); //return array of each object's key-value pairs
+            for (const [key, value] of entries1.sort()) { //loop through each element (key-value) in the array
+                let keyword = `${key}`; //the different key properties
+                let values = `${value}`; //the different value properties
+                if(values.includes(`${queryString}`) === true && returnEditions.includes(editionsList[i]) === false) {
+                    returnEditions.push(editionsList[i]);
+                }
+            }
+        }
+      }
+    }
+    console.log(returnEditions) // returns an array of the editions which the query is found in
 }
-
-
-// clean up array
-function formatArray(arr) {
-    arr.sort();
-    return arr.filter((value, index) => arr.indexOf(value) === index);
-}
+// SUPER SLOW SEARCH, NEED TO IMPLEMENT SEARCH VALIDATION AND REDUCE TIME
