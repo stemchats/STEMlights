d1babc504004070e444bd18108cab011687cad42
anastasia21112 <anad221122@gmail.com>
2021-06-18 00:55:19 -0600

    webscrape tests

---
+++ web-scraping/scrape.js
@@ -0,0 +1,13 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+request('https://stemchats1.createsend.com/campaigns/reports/viewCampaign.aspx?d=t&c=9D48DA2F8213BDDE&ID=2999BD115C61677B2540EF23F30FEDED&temp=False&tx=0&source=SnapshotHtml', (error, response, html) => {
+    if(!error && response.statusCode == 200)
+    {
+        const $ = cheerio.load(html);
+
+        const siteHeading = $('.layout__inner');
+
+        console.log(siteHeading.html())
+    }
+});
